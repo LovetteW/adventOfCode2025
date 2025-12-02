aed5012d8b6935eb69e9fe76685f41f92f8e672a
[Lovette Watts] <lovettewatts@gmail.com>
2025-12-02 10:21:27 -0500

    Day 2: functional character iterator

--- 2wo/Day2_IDtester.c
+++ 2wo/Day2_IDtester.c
@@ -6,8 +6,10 @@ int main(){
 		printf("Couldn't open file.\n");
 		return 1;
 	}
-	//The fgetc() function returns the ASCII value of the character at the current position in the file and then moves the position indicator to the next character.[https://www.w3schools.com/c/ref_stdio_fgetc.php] 
-	char c = fgetc(rangesFile);
-	printf("%c", c);
+	//The fgetc() function returns the ASCII value of the character at the current position in the file and then moves the position indicator to the next character.[https://www.w3schools.com/c/ref_stdio_fgetc.php]
+	char c;
+	while((c = fgetc(rangesFile)) != EOF){ //you NEED to do c = fgetc() so that each character gets assigned to c. The incrementing to the next char is done by the function itself
+		printf("%c", c);
+		}
 	return 0;
 }
