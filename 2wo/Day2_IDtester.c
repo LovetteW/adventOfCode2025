1212b983ab1022b3c62e9853170f7da0246b7411
[Lovette Watts] <lovettewatts@gmail.com>
2025-12-05 00:45:58 -0500

    cleanup

--- 2wo/Day2_IDtester.c
+++ 2wo/Day2_IDtester.c
@@ -2,64 +2,59 @@
 #include <string.h>
 #include <stdlib.h>
 
-/*here's a cool thing that happened during development (output of vim -r <file>):
-Using swap file "~/Documents/adventOfCode2025/2wo/.Day2_IDtester.c.swp"
-Original file "~/Documents/adventOfCode2025/2wo/Day2_IDtester.c"
-Recovery completed. You should check if everything is OK.
-(You might want to write out this file under another name
-and run diff with the original file to check for changes)
-You may want to delete the .swp file now.*/
+// Don't forget you need your \0 terminators at the end of strings.
 
 int main(){
 	int sum = 0;
 	int *s = &sum;
+
+	//declare a pointer for a file-type data using the return value of the fopen() function
 	FILE *rangesFile = fopen("Product ID Ranges.txt", "r");
+	//read the value of rangesFile (for a pointer it'll be a valid 0x/ hex address or NULL/0) and if not the valid thing then oopsie, let 'em know we got a problem
 	if(!rangesFile){
 		printf("Couldn't open file.\n");
 		return 1;
 	}
-	
-	//get the length of this big ol' single string we're working with. I'm finna implement a functionality that let's us recognize non-numeric characters, signalling a completion of a lower||upper bound, blah blah blah...
-	char input[519]; //517 chars + \n + \0. Reducing array size to 518 yields a later inputLength of 517... How curious! Upper-bounds testing confirms the TRUE strlen(input) is 518 because it doesn't increase if we give the array more bytes.
+	//prepare a character variable to store that single long string we see in the input file [https://pastebin.com/Uz63VRqW]
+	char input[2550];
+
 	fgets(input, sizeof(input), rangesFile);
-	printf("%s", input);
-
-
+	printf("Input:\n%s\n", input);
 	int inputLength = strlen(input);
-	printf("\nInput length: %d\n", inputLength); 
-	//The fgetc() function returns the ASCII value of the character at the current position in the file and then moves the position indicator to the next character.[https://www.w3schools.com/c/ref_stdio_fgetc.php]
+	printf("Input length: %d\n", inputLength); 
 	int index = 0;
-	char bufferThatEventuallyTurnsIntoTheFirstProductID[20];
+	char bufferThatEventuallyTurnsIntoTheFirstProductID[255];
 	int bufferIndex = 0;
 	int lower;
-	char bufferThatMaybeOneDayWillGrowUpToBeSantaClause[20];
+	char bufferThatMaybeOneDayWillGrowUpToBeSantaClause[255];
 	int upper;
 	char *theCutestPointerThatEverWas = bufferThatEventuallyTurnsIntoTheFirstProductID;
 
-	//feed an integer within the product range
 	int isLeadingZero(int productIDnum){
-		char productID[20];
+		char productID[255];
 		itoa(productIDnum, productID, 10);
 		if(productID[0] == '0' || productID[0] == 0){
-			printf("LEADING ZERO FOUND!#################################################################");
+			printf("LEADING ZERO FOUND!");
 			return 1;
 		} else {
 			return 0;
 		}
 	}
-	//input is an array
+	
 	int isRepeater(int productIDnum){
-		char productID[20];
+		char productID[255];
 		itoa(productIDnum, productID, 10);
 		int i = 0;
 		int length = strlen(productID);
-		char firstHalf[10];
-		char secondHalf[10];
+		char firstHalf[255];
+		char secondHalf[255];
 		for(; i <= length; i++){
 			firstHalf[i] = productID[i];
 			i++;
 			while(i >= length/2){
 				secondHalf[i-(length/2)] = productID[i];
+				i++;
+
 			}
 		}
 		printf("firstHalf: %c | secondHalf: %c", firstHalf[0], secondHalf[0]);
